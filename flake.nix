b0522fde4856913abac47efc3d80ae1b55b9bfa1
Martin Larsson <larssonmartin1998@gmail.com>
2025-05-15 00:57:08 +0200

    feat: Export shellHook for easier integration w/ other flakes.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Patch compile_commands.json with Nix's include flags from $NIX_CFLAGS_COMPILE";
+  description = "Add $NIX_CFLAGS_COMPILE include flags to compile_commands.json";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,23 +12,50 @@
       nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.writeShellApplication {
-          name = "patch-compile-commands";
-          runtimeInputs = [ pkgs.python3 ];
-          text = ''
-            exec ${pkgs.python3}/bin/python ${self}/patch_cc.py "$@"
-          '';
-        };
+    let
+      shellHookText = ''
+        _patch_cc() {
+          [[ -z "$NIX_CFLAGS_COMPILE" ]] && return
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.default;
-        };
-      }
-    );
+          # If the user set PATCH_CC_DB, forward it as argv[1]; otherwise no arg (python script handles this fallback)
+          if [[ -n "''${PATCH_CC_DB:-}" ]]; then
+            patch-compile-commands "''${PATCH_CC_DB}"
+          else
+            patch-compile-commands
+          fi
+        }
+
+            _patch_cc
+            if [[ -n "$PROMPT_COMMAND" ]]; then
+              export PROMPT_COMMAND="_patch_cc;$PROMPT_COMMAND"
+            else
+              export PROMPT_COMMAND="_patch_cc"
+            fi
+      '';
+
+      perSystem = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          patchBin = pkgs.writeShellApplication {
+            name = "patch-compile-commands";
+            runtimeInputs = [ pkgs.python3 ];
+            text = ''
+              exec ${pkgs.python3}/bin/python ${self}/patch_cc.py "$@"
+            '';
+          };
+        in
+        {
+          packages.default = patchBin;
+          apps.default = flake-utils.lib.mkApp {
+            drv = patchBin;
+          };
+        }
+      );
+    in
+    perSystem
+    // {
+      lib.shellHook = shellHookText;
+    };
 }
